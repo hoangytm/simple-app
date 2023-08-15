0f78fd5b6edb91b8b4dc67be38f01fa4458edd0b
Hoang Phan <51194076+hoangytm@users.noreply.github.com>
2023-08-15 16:02:33 +0700

    Update TestController.java

--- src/main/java/com/okta/developer/simpleapp/TestController.java
+++ src/main/java/com/okta/developer/simpleapp/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/greeting")
     public String greet() {
+        String a = "1";
         return "Hello ";
     }
 }
